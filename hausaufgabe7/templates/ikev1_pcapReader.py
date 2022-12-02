79acca64dbb99107d3a82e99310d68b4fc52a8e4
KrokoNinja <79910460+KrokoNinja@users.noreply.github.com>
2022-12-02 14:20:50 +0100

    remove unnecessary function PaketList()

--- hausaufgabe7/templates/ikev1_pcapReader.py
+++ hausaufgabe7/templates/ikev1_pcapReader.py
@@ -1,7 +1,7 @@
 from scapy.all import *
 def openPCAPFile(path: str): #-> scapy.plist.PacketList:
     #TODO Read a pcap or pcapng file and return a packet list
-    return PacketList(rdpcap(path))
+    return rdpcap(path)
 
 def getISAKMPPackets(packets: scapy.plist.PacketList): #-> []
     #TODO returns a list containing only the ISAKMP Layers of the packets in packetList
